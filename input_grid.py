ed84763cd1fedd1b08533a78b397d13f7ddc4662
Robert Fielding <robert.fielding@hotmail.co.uk>
2021-05-02 13:59:50 +0100

    First Working

---
+++ input_grid.py
@@ -0,0 +1,85 @@
+import tkinter
+from tkinter import *
+
+import numpy as np
+from PIL import ImageGrab
+from keras.models import load_model
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
+from matplotlib.figure import Figure
+
+canvas_size = 3 * 28
+resized_img_size = (28, 28)
+nn_model = load_model('neural_network_model.h5')
+
+
+class DigitClassifier:
+    def __init__(self, master, canvas_dim, image_size):
+        self.master = master
+        descriptor = Label(self.master, text="Press and Drag the mouse to draw")
+        descriptor.pack(side=BOTTOM)
+
+        self.image_size = image_size
+        self.master.title('Interactive Digit Classifier')
+
+        self.canvas = Canvas(master,
+                             width=canvas_dim,
+                             height=canvas_dim,
+                             background='white',
+                             highlightthickness=0
+                             )
+        self.canvas.pack()
+
+        self.canvas.bind('<B1-Motion>', self.paint)
+
+        classify_button = Button(master, text='Classify Digit', command=self.classify_digit)
+        classify_button.pack()
+        clear_button = Button(master, text='Clear', command=self.clear_canvas)
+        clear_button.pack()
+
+        # Figure to display bar graph of probabilities
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.plot = FigureCanvasTkAgg(self.fig, master=master)
+
+    def clear_canvas(self):
+        self.clear_plot()
+        self.canvas.delete('all')
+
+    def paint(self, event):
+        x1, y1 = (event.x - 2), (event.y - 2)
+        x2, y2 = (event.x + 2), (event.y + 2)
+        self.canvas.create_oval(x1, y1, x2, y2, fill='black')
+
+    def capture_canvas(self):
+        x = self.master.winfo_rootx() + self.canvas.winfo_x()
+        y = self.master.winfo_rooty() + self.canvas.winfo_y()
+        x1 = x + self.canvas.winfo_width()
+        y1 = y + self.canvas.winfo_height()
+        im = ImageGrab.grab((x, y, x1, y1))
+        im.save("trial.jpg")
+        return im.resize(self.image_size)
+
+    def clear_plot(self):
+        self.ax.clear()
+        self.ax.set_xticks(list(range(10)))
+        self.plot.draw()
+
+
+    def plot_bar(self, pred_prob):
+        self.ax.set_xticks(list(range(10)))
+        self.ax.bar(np.arange(0, 10, 1), pred_prob)
+        self.plot.draw()
+        self.plot.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    def classify_digit(self):
+        canvas = self.capture_canvas()
+        canvas_array = np.reshape(255 - np.array(canvas.convert("L")), (1, 28, 28, 1))
+        assert canvas_array.shape == (1, 28, 28, 1)
+        pred_prob = nn_model.predict_proba(canvas_array)[0]
+        self.clear_plot()
+        self.plot_bar(pred_prob)
+
+
+master = Tk()
+input_grid = DigitClassifier(master, canvas_size, resized_img_size)
+master.mainloop()
---
+++ neural_network.py
@@ -0,0 +1,53 @@
+import keras
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+EPOCHS = 50
+BATCH_SIZE = 2048
+
+print('Loading data')
+(features_train, labels_train), (features_test, labels_test) = keras.datasets.mnist.load_data()
+print('Finished loading data')
+
+datagen = ImageDataGenerator(
+    rotation_range=5,
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+input_shape = (28, 28, 1)
+model = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3)),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ]
+)
+
+model.compile(optimizer="adam",
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+features_train = np.expand_dims(features_train, -1)
+labels_train = np.expand_dims(labels_train, -1)
+features_test = np.expand_dims(features_test, -1)
+labels_test = np.expand_dims(labels_test, -1)
+
+print("Start Training")
+
+batch = 0
+for x_batch, y_batch in datagen.flow(features_train, labels_train, batch_size=BATCH_SIZE):
+    model.fit(x_batch, y_batch, validation_data=(features_test, labels_test))
+    batch += 1
+    if batch > len(features_train) // BATCH_SIZE:
+        break
+print('Finished training')
+
+print(model.evaluate(datagen.flow(features_test, labels_test)))
+model.save('neural_network_model.h5')
